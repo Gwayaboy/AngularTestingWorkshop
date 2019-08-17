cf6d62c0aa6e190a125a2a5aae480df0c79fba34
Franck Théolade <frtheola@microsoft.com>
2019-08-17 18:40:46 +0100

    Parameterisation

--- TourOfHeroesApp/src/app/strength/strength.pipe.spec.ts
+++ TourOfHeroesApp/src/app/strength/strength.pipe.spec.ts
@@ -1,21 +1,20 @@
 import { StrengthPipe } from './strength.pipe';
 
 describe('StrengthPipe', () => {
-
-  const parameters = [
+  const testCases = [
     { strength: 1, expectedResult: '1 (weak)'},
     { strength: 10, expectedResult: '10 (strong)'},
     { strength: 100, expectedResult: '100 (unbelievable)'},
   ];
 
-  parameters.forEach((param) => {
-    it(`should display "${param.expectedResult}" when strength is ${param.strength}` , () => {
+  testCases.forEach(({strength, expectedResult}) => {
+    it(`should display "${expectedResult}" when strength is ${strength}` , () => {
       // Arrange
       const pipe = new StrengthPipe();
       // Act
-      const result = pipe.transform(param.strength);
+      const result = pipe.transform(strength);
       // Assert
-      expect(result).toEqual(param.expectedResult);
+      expect(result).toEqual(expectedResult);
     });
   });
 });
